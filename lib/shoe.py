61305fe3da6691387831a00a9144e76b34d7fb71
Collins Kibet <kollcibe05@gmail.com>
2025-11-15 14:26:49 +0300

    solved shoe case

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
-class Shoe:    
+class Shoe:
+    def __init__(self, brand, size, condition="managable"):
+        self.brand = brand
+        self.size = size 
+        self.condition = condition 
+    @property
+    def size(self):
+        return self._size
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")              
     pass
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"  
+
+shoe = Shoe("Louis Vuitton", 8, "")    
+print(shoe.brand)
+print(shoe.size)
+print(shoe.cobble())
+
+
